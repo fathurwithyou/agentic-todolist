0c55a02398b520a5ecd0c12131d4e61ecee50d3e
ahargunyllib <billy.bpm03@gmail.com>
2025-08-22 22:53:01 +0700

    style: format code and update configuration files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
+// biome-ignore lint/style/noNonNullAssertion: This is a known safe usage
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
