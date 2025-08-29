1b35ace1449102f1a9bc4825ec65635b0f4cf800
Fathur <rizkyfathur326@gmail.com>
2025-08-30 02:30:29 +0700

    feat: add mutations for saving and testing API keys, and querying providers

--- frontend/src/shared/repositories/api-key/query.ts
+++ frontend/src/shared/repositories/api-key/query.ts
@@ -1,9 +1,31 @@
-import { useQuery } from "@tanstack/react-query";
-import { getListApiKeys } from "./action";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getListApiKeys, saveApiKey, testApiKey, getListProviders } from "./action";
+import { type SaveApiKeyRequest } from "./dto";
 
 export const useGetListApiKeysQuery = () => {
   return useQuery({
     queryKey: ["api-keys"],
     queryFn: getListApiKeys,
+  });
+};
+
+export const useSaveApiKeyMutation = () =>
+	useMutation({
+		mutationFn: async (data: SaveApiKeyRequest) => {
+			return await saveApiKey(data);
+		},
+	});
+
+export const useTestApiKeyMutation = () =>
+	useMutation({
+		mutationFn: async (data: { provider: string }) => {
+			return await testApiKey(data);
+		},
+	});
+
+export const useGetListProvidersQuery = () => {
+  return useQuery({
+    queryKey: ["api-providers"],
+    queryFn: getListProviders,
   });
 };
